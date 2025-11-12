19486a43f00ce8045e048cfd451487d4488f0247
Glenn Bech <glenn.bech@gmail.com>
2025-11-12 21:16:07 +0000

    wip

---
+++ modules/s3-website/variables.tf
@@ -0,0 +1,15 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "website_files_path" {
+  description = "Path to website files to upload"
+  type        = string
+}
